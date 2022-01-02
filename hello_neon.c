a476f4a0647ee2f14d17d65f7af66d3dd1a4e1b1
consti10 <geierconstantinabc@gmail.com>
2022-01-02 19:56:20 +0100

    init

--- hello_neon.c
+++ hello_neon.c
@@ -6,11 +6,23 @@
 #include <arm_neon.h>
 
 
+uint32x4_t double_elements(uint32x4_t input)
+{
+    return(vaddq_u32(input, input));
+}
+
+
 /** \brief a hello program for testing NEON availability
  */
 
 int main()
 {
-    printf("Hello");
+    printf("Hello\n");
+
+    uint32x4_t input,res;
+
+    res= double_elements(input);
+
+
     return 0;
 }
