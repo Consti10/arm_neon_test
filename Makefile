84f7a96289a644e4636065bbf8a390c22ef0c75d
consti10 <geierconstantinabc@gmail.com>
2022-01-02 19:50:53 +0100

    init

---
+++ Makefile
@@ -0,0 +1,12 @@
+# Test on rpi
+
+_CFLAGS := $(CFLAGS) -O2 -mfpu=neon -march=armv7-a -marm
+
+all: hello_neon
+
+
+hello_neon: hello_neon.c
+	$(CC) $(_CFLAGS) -o hello_neon hello_neon.c
+
+clean:
+	rm -rf hello_neon
---
+++ hello_neon.c
@@ -0,0 +1,16 @@
+//
+// Created by consti10 on 02.01.22.
+//
+
+#include <stdio.h>
+#include <arm_neon.h>
+
+
+/** \brief a hello program for testing NEON availability
+ */
+
+int main()
+{
+    printf("Hello");
+    return 0;
+}
