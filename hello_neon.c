47c88045c287043959fedd6e58ee543556a5e649
consti10 <geierconstantinabc@gmail.com>
2022-01-02 19:58:16 +0100

    init

--- hello_neon.c
+++ hello_neon.c
@@ -11,6 +11,15 @@ uint32x4_t double_elements(uint32x4_t input)
     return(vaddq_u32(input, input));
 }
 
+void lookup(){
+    uint8x8_t out_uint8x8_t;
+    uint8x8_t arg0_uint8x8_t;
+    uint8x8_t arg1_uint8x8_t;
+    uint8x8_t arg2_uint8x8_t;
+
+    out_uint8x8_t = vtbx1_u8 (arg0_uint8x8_t, arg1_uint8x8_t, arg2_uint8x8_t);
+}
+
 
 /** \brief a hello program for testing NEON availability
  */
@@ -22,7 +31,7 @@ int main()
     uint32x4_t input,res;
 
     res= double_elements(input);
-
+    lookup();
 
     return 0;
 }
